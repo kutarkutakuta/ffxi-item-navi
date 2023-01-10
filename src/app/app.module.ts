a67a3e3205783701649d44af83ad1ba5e76e286d
yuma nagashima <yuma.nagashima@gmail.com>
2023-01-11 01:01:14 +0900

    install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { ja_JP } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import ja from '@angular/common/locales/ja';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(ja);
 
 @NgModule({
   declarations: [
@@ -10,9 +19,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: ja_JP }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

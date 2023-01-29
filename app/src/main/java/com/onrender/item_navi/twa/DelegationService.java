43ce1bf462f173688627c6095d0df062e4a22b13
kutarkutakuta <kutarkutakuta@gmail.com>
2023-01-29 10:48:17 +0900

    PWA

---
+++ app/src/main/java/com/onrender/item_navi/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.onrender.item_navi.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
